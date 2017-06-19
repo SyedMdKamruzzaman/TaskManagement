347872da1d140917e822dc2e82e450286ea4591b
Abdullah <jewel100@gmail.com>
2017-06-19 13:54:51 +0600

    1

---
+++ TaskManagement/Controllers/TaskController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskManagement.Models;
+
+namespace TaskManagement.Controllers
+{
+    public class TaskController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Task
+        public ActionResult Index()
+        {
+            return View(db.TaskModels.ToList());
+        }
+
+        // GET: Task/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // GET: Task/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Task/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,description,dateCreated,dateUpdated")] TaskModel taskModel)
+        {
+            if (ModelState.IsValid)
+            {
+                taskModel.dateCreated = DateTime.Now;
+                taskModel.dateUpdated = DateTime.Now;
+                db.TaskModels.Add(taskModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taskModel);
+        }
+
+        // GET: Task/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,name,description,dateCreated,dateUpdated")] TaskModel taskModel)
+        {
+            if (ModelState.IsValid)
+            {
+                DbEntityEntry<TaskModel> dbEntity = db.Entry(taskModel);
+                dbEntity.State = EntityState.Modified;
+                dbEntity.Property(db => db.dateCreated).IsModified = false;                
+                taskModel.dateUpdated = DateTime.Now;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taskModel);
+        }
+
+        // GET: Task/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            TaskModel taskModel = db.TaskModels.Find(id);
+            db.TaskModels.Remove(taskModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TaskManagement/Models/TaskModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagement.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public Int64 id { get; set; }
+
+        [Required]
+        [Display(Name = "Task Name", Prompt = "Please enter task name")]
+        public string name { get; set; }
+
+        [Required]
+        [Display(Name = "Task Description", Prompt = "Please enter task description")]
+        public string description { get; set; }
+
+        [ScaffoldColumn(false)]
+        [DataType(DataType.DateTime)]
+        public DateTime? dateCreated { get; set; }
+
+        [ScaffoldColumn(false)]
+        [DataType(DataType.DateTime)]
+        public DateTime dateUpdated { get; set; }
+    }
+}
